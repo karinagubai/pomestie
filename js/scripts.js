054ca6300e05acc350f62ad75363655ea29bd98f
Карина Губайдуллина <karina@MacBook-Air-Karina.local>
2019-09-10 18:01:15 +0500

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+// show-hide main-nav on small devices
+
+$('.open-nav').on('click', function(){
+  $('.main-nav').css("display", "block");
+});
+$('.close-nav').on('click', function(){
+  $('.main-nav').hide();
+});
+
+$('.main-nav').on('click', 'a', function(){
+  $('.main-nav').hide();
+});
+
+$(window).on('scroll', function(){
+  if ($(window).scrollTop() > 50) {
+          $('.main-header').addClass('sticky');
+      } else {
+          $('.main-header').removeClass('sticky');
+      }
+});
+
+$(window).resize(function(){
+  if (window.innerWidth > 991) {
+        $('.main-nav').css("display", "flex");
+    } else {
+        $('.main-nav').hide();
+    }
+});
+
+// carousel
+
+$('.right').on('click', function(){
+  $('.project:first').insertAfter('.project:last');
+});
+$('.left').on('click', function(){
+  $('.project:last').insertBefore('.project:first');
+});
